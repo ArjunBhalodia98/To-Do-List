70649ebdd6bf2ca25f6a370b67a6c40f65bc5e1c
Arjun Bhalodia - Mac <thisajplays@gmail.com>
2021-08-10 12:15:23 -0400

    Initial Push

---
+++ To-Do-List/To-Do-List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To-Do-List
+//
+//  Created by Arjun Bhalodia on 8/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
